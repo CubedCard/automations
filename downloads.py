b1e9b8ab73fbc84eb9c03a6853784eb4d632a78e
J. Derksen <jip.derksen@hva.nl>
2023-12-28 14:58:27 +0100

    initial commit

---
+++ downloads.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+def organize_downloads(download_folder, destination_folder):
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
+
+    files = os.listdir(download_folder)
+
+    for file in files:
+        file_path = os.path.join(download_folder, file)
+
+        if os.path.isfile(file_path):
+            _, extension = os.path.splitext(file)
+
+            extension = extension[1:]
+
+            extension_folder = os.path.join(destination_folder, extension)
+            if not os.path.exists(extension_folder):
+                os.makedirs(extension_folder)
+
+            shutil.move(file_path, os.path.join(extension_folder, file))
+
+if __name__ == "__main__":
+    downloads_folder = "/Users/jipderksen/Downloads"
+    destination_folder = "/Users/jipderksen/Downloads/files"
+
+    organize_downloads(downloads_folder, destination_folder)
---
+++ new_file.py
@@ -0,0 +1,31 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import subprocess
+
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            if event.src_path.endswith('.crdownload') or 'com.brave.Browser' in event.src_path:
+                print(f'File {event.src_path} is still downloading.')
+            else:
+                print(f'New file created: {event.src_path}')
+                subprocess.run(['python3', 'downloads.py', event.src_path])
+
+def monitor_downloads():
+    path = '/Users/jipderksen/Downloads'
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+if __name__ == "__main__":
+    monitor_downloads()
+
