75dd48a309d9301c7b10453efdf463ef2e639207
J. Derksen <jip.derksen@hva.nl>
2024-02-02 19:47:30 +0100

    improved the automated number memory to not screw up the 7 and 1

--- games/humanbench/memory.py
+++ games/humanbench/memory.py
@@ -1,14 +1,20 @@
+import cv2
 from PIL import ImageGrab
 import pytesseract
 import pyautogui
+import numpy as np
 
 top_left = (410, 400)
 bottom_right = (1371, 507)
 pyautogui.PAUSE = 0
 
 while True:
-    img = ImageGrab.grab(bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
-    result = pytesseract.image_to_string(img, lang='eng', config='--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789')
+    screenshot = ImageGrab.grab(bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
+    image = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    result = pytesseract.image_to_string(img_gray, lang='eng', config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789 --dpi 70')
+
     if result.strip().isdigit():
         print(result.strip())
         r, g, b = 56, 109, 182
@@ -18,3 +24,4 @@ while True:
             pyautogui.press(char)
         pyautogui.press('enter')
         pyautogui.press('enter')
+
