8502982d96992c5d3bba43bac5959f9850937abe
J. Derksen <jip.derksen@hva.nl>
2024-02-02 15:52:32 +0100

    improved the automated aim game script

--- games/humanbench/aim.py
+++ games/humanbench/aim.py
@@ -1,24 +1,32 @@
 import pyautogui
 from PIL import ImageGrab
-import keyboard 
+import numpy as np
+import time
 
-top_left = (404, 238)
+top_left = (473, 247)
 bottom_right = (1365, 717)
+start_button = (888, 462)
 pyautogui.PAUSE = 0
 
 def aim():
-    screen = ImageGrab.grab(bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
-    width, height = screen.size
-    for x in range(width):
-        for y in range(height):
-            r, g, b, _ = screen.getpixel((x, y))
-            xr, xg, xb = 163, 195, 229
-            if r == xr and g == xg and b == xb:
-                pyautogui.moveTo(x + top_left[0], y + top_left[1])
-                pyautogui.click()
-                return 
+    clicks_left = 30
+    while clicks_left > 0:
+        screenshot = ImageGrab.grab(bbox=(top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
+        screenshot_array = np.array(screenshot)
 
-while True:
-    if keyboard.is_pressed('q'):
-        break
+        target_color = (163, 195, 229, 255)
+
+        if np.any(np.all(screenshot_array == np.array(target_color), axis=-1)):
+            target_position = np.argwhere(np.all(screenshot_array == np.array(target_color), axis=-1))[0]
+            x, y = target_position[1], target_position[0]
+            pyautogui.click(x + top_left[0], y + top_left[1])
+            clicks_left -= 1
+        else:
+            print("Target color not found in the screenshot.")
+            break
+
+if __name__ == "__main__":
+    time.sleep(1)
+    pyautogui.moveTo(start_button[0], start_button[1])
+    pyautogui.click()
     aim()
